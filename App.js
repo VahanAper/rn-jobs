347414b320b87c8ecf21a79800a47a05f73452df
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-08 23:10:45 +0400

    add simple Tab navigation

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+    TabNavigator,
+} from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+const MainNavigator = TabNavigator({
+    welcome: { screen: WelcomeScreen },
+    auth: { screen: AuthScreen },
+});
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <MainNavigator />
+        );  
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
