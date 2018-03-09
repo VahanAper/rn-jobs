82c9d8a9436be48cd710582be16867689eed0319
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-10 00:20:19 +0400

    style Slides component

--- components/Slides.js
+++ components/Slides.js
@@ -2,16 +2,53 @@ import React from 'react';
 import {
     View,
     Text,
+    ScrollView,
+    Dimensions,
 } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
 class Slides extends React.Component {
+    renderSlides = () => {
+        return this.props.data.map((slide) => {
+            return (
+                <View
+                    key={slide.id}
+                    style={[
+                        styles.slideStyle,
+                        { backgroundColor: slide.color },
+                    ]}
+                >
+                    <Text style={styles.textStyle}>{slide.text}</Text>
+                </View>
+            );
+        });
+    }
+    
     render() {
         return (
-            <View>
-                <Text>Slider</Text>
-            </View>
+            <ScrollView
+                horizontal
+                pagingEnabled
+                style={{ flex: 1 }}
+            >
+                {this.renderSlides()}
+            </ScrollView>
         );
     }
 }
+
+const styles = {
+    textStyle: {
+        fontSize: 30,
+        color: 'white',
+    },
+    slideStyle: {
+        flex: 1,
+        width: SCREEN_WIDTH,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+};
 
 export default Slides;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {
-    View,
-    Text,
-} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
-    { text: 'Welcome to JobApp' },
-    { text: 'Set your location, then swipe away' },
+    {
+        id: 1,
+        color: '#03A9F4',
+        text: 'Welcome to JobApp'
+    }, {
+        id: 2,
+        color: '#009688',
+        text: 'Set your location, then swipe away'
+    }, {
+        id: 3,
+        color: '#03A9F4',
+        text: 'Use this to get a job'
+    }
 ];
 
 class WelcomeScreen extends React.Component {
     render() {
-        return (
-            <Slides data={SLIDE_DATA} />
-        );
+        return (<Slides data={SLIDE_DATA}/>);
     }
 }
 
