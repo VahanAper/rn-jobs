2431bec2a20f5b443ba829e8f18aff37d1afc7c8
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-10 22:52:54 +0400

    add initial region for the MapView

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,11 +8,21 @@ import {
 } from 'expo';
 
 class MapScreen extends React.Component {
+    state = {
+        region: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        },
+    }
+    
     render() {
         return (
             <View style={{ flex: 1 }}>
                 <MapView
                     style={{ flex: 1 }}
+                    region={this.state.region}
                 />
             </View>
         );
