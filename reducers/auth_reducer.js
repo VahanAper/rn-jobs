2196671274df0587ebb8c318ed875d6b1fd529f3
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-10 18:50:06 +0400

    add auth reducer

---
+++ reducers/auth_reducer.js
@@ -0,0 +1,21 @@
+import {
+    FB_LOGIN_FAIL,
+    FB_LOGIN_SUCCESS,
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FB_LOGIN_SUCCESS:
+            return {
+                token: action.payload,
+            };
+            
+        case FB_LOGIN_FAIL:
+            return {
+                token: null,
+            };
+            
+        default:
+            return state;
+    }
+};
