f592779b54095ecc939a2c860cdf92eb844ba07c
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-11 16:13:51 +0400

    add renderCard method

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -6,20 +6,79 @@ import {
 import {
     connect,
 } from 'react-redux';
+import {
+    MapView,
+} from 'expo';
+import {
+    Card,
+    Button,
+} from 'react-native-elements';
 
 import Swipe from '../components/Swipe';
 
 class DeckScreen extends React.Component {
+    clearSnippetText = (snippet) => {
+        return snippet.replace(/<b>/g, '').replace(/<\/b>/g, '');
+    }
+    
+    renderCard = (job) => {
+        const initialRegion = {
+            longitude: job.longitude,
+            latitude: job.latitude,
+            // these props specifies zoom level
+            longitudeDelta: 0.045,
+            latitudeDelta: 0.02,
+        }
+        return (
+            <Card title={job.jobtitle}>
+                <View style={{ height: 200 }}>
+                    <MapView
+                        cacheEnabled 
+                        style={{ flex: 1 }}
+                        scrollEnabled={false}
+                        initialRegion={initialRegion}
+                    />
+                </View>
+                
+                <View style={styles.detailWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                
+                <Text>
+                    {this.clearSnippetText(job.snippet)}
+                </Text>
+            </Card>
+        );
+    }
+    
+    renderNoMoreCards = () => {
+        return (
+            <Card title="No more jobs" />
+        );
+    }
+    
     render() {
         return (
             <View>
                 <Swipe
                     data={this.props.jobs}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
                 />
             </View>
         );
     }
 }
+
+const styles = {
+    detailWrapper: {
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+};
 
 const mapStateToProps = ({ jobs }) => ({
     jobs: jobs.results,
